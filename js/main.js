99b5577a0c6c21a97d22194a2e3e961d2accec9e
Pavel <akulov_pavel88@mail.ru>
2018-02-14 15:19:28 +0200

    2d and 3d geometry

--- js/main.js
+++ js/main.js
@@ -41,7 +41,7 @@ window.onload= function(){
 		});
 
 		meshes2.forEach(function(elem,index){
-			if(index<200){
+			if(index<220){
 				elem.position.y = -10;
 				elem.position.z = -200;
 				elem.position.x = 150;
@@ -62,7 +62,7 @@ window.onload= function(){
 			}
 		});
 		meshes.forEach(function(elem,index){
-			if(index<200){
+			if(index<220){
 				elem.position.y = -120;
 				elem.rotation.y=1;
 				elem.material=new THREE.MeshNormalMaterial();
@@ -82,6 +82,35 @@ window.onload= function(){
 			elem.rotation.x+=0.01;
 		})
 	}
+
+	var vertex = new THREE.Vector3();
+	vertex.x = -120;
+	vertex.y = 120;
+	vertex.z = 50;
+	var vertex2 = new THREE.Vector3();
+	vertex2.x = -150;
+	vertex2.y = 0;
+	vertex2.z = -50;
+	//Создаем геометрию
+	var geometry = new THREE.Geometry();
+	//В список вершин геометрии заносим выше созданную точку
+	geometry.vertices.push( vertex);
+	geometry.vertices.push( vertex2 );
+	geometry.colors.push(0xffff00);
+	material = new THREE.PointsMaterial( { size: 215, color:'rgb(14, 235, 153)'} );
+	particles = new THREE.ParticleSystem( geometry, material );
+	particles.sortParticles = true;
+	scene.add(particles);
+
+
+	var planeGeometry = new THREE.PlaneGeometry(60,20,1,1);
+	var planeMaterial = new THREE.PointsMaterial( {color: 0xffff00,side: THREE.DoubleSide} );
+	var plane = new THREE.Mesh( planeGeometry, planeMaterial );
+	plane.rotation.x=-0.5;
+	plane.position.x = -120;
+	plane.position.y = -120;
+	plane.position.z = 0;
+	scene.add(plane);
 
 
 	var controls = new THREE.TrackballControls(camera);
